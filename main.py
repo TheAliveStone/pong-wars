ffad8f2e17099a3c83de59ceedabd22425715c9f
Livingstone <160360410+TheAliveStone@users.noreply.github.com>
2025-07-17 08:37:20 +0000

    General pygame setup

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+
+# General setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# Keep the game running
+running = True
+while running:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the display surface with a color
+    display_surface.fill("blue")  # Fill with blue
+
+    # Update the display
+    pygame.display.flip()
+
+# Quit pygame    
+pygame.quit()
